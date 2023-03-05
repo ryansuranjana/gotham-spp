53a32f3e9b3d7b726cf7e1886ac17d076259564a
ryansuranjana <ryansuranjana23@gmail.com>
2023-03-05 22:28:59 +0800

    fix content in navbar and login

--- app/views/pages/auth/login.php
+++ app/views/pages/auth/login.php
@@ -49,7 +49,7 @@
                             <div class="col-lg-6">
                                 <div class="p-5">
                                     <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Welcome to the Gotham School tuition payment website</h1>
+                                        <h1 class="h4 text-gray-900 mb-4">Selamat Datang di Website Pembayaran SPP Gotham</h1>
                                     </div>
                                     <?php Flasher::flash(function($flash) { ?>
                                         <div class="alert alert-danger" role="alert">
